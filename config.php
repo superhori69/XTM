955e321af40a665b148e2536b150618bbe565a9c
Xan.Lua <annalover179@gmail.com>
2022-10-12 19:51:34 +0300

    Finished the setup and updated config.php.

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
     session_start();
 
+    //Dummy data until gets completed with actual credentials.
     define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'taskmanager');
-    define('SITEURL', 'http://192.168.0.227/tm/');
+    define('DB_USERNAME', 'task');
+    define('DB_PASSWORD', 'pass123');
+    define('DB_NAME', 'xtaskmanager');
+    define('SITEURL', 'http://127.0.0.1/xtm/');
